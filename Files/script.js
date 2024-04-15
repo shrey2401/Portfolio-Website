5e25448e448ce9e29348cbf353d8d5598a4f64e2
Shrey Patel <shrey0107@gmail.com>
2024-04-15 01:34:18 -0400

    Feature added in contact form

--- Files/script.js
+++ Files/script.js
@@ -13,9 +13,8 @@ window.onscroll = () => {
     if (top >= offset && top < offset + height) {
       navLinks.forEach((links) => {
         links.classList.remove("active");
-        document
-          .querySelectorAll("header nav a [href*=" + id + "]")
-          .classList.add("active");
+        document.querySelector("header nav a[href*=" + id + "]")
+        .classList.add("active");
       });
     }
   });
@@ -24,3 +23,40 @@ menuIcon.onclick = () => {
   menuIcon.classList.toggle("bx-x");
   navbar.classList.toggle("active");
 };
+
+const form = document.querySelector('form')
+const Fullname = document.getElementById('name')
+const email = document.getElementById('email')
+const phoneNum = document.getElementById('phone')
+const msg = document.getElementById('message')
+const subject = document.getElementById('subject')
+
+
+function sendEmail(){
+  const bodymsg = `Full Name : ${Fullname.value}<br> Email : ${email.value}<br>Phone Number : ${phoneNum.value}<br>Subject : ${subject.value}<br>  Message : ${msg.value}<br>`
+
+  Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "shrey0107@gmail.com",
+    Password : "A78756FE5B5F0BA9EEC83C5A510CD86F47E0",
+    To : 'shrey0107@gmail.com',
+    From : "shrey0107@gmail.com",
+    Subject : subject.value,
+    Body : bodymsg
+  }).then(
+  message => {
+    if(message == "OK"){
+      Swal.fire({
+        title: "Success!",
+        text: "Your message has been Sent",
+        icon: "success"
+      });
+    }
+    }
+  );
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  sendEmail();
+})
